96dce4fff3ae3c6c94d756f7b1348cf7069ecb4c
s145469 <luud-@live.nl>
2019-03-20 16:07:51 +0100

    Send pinstates

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,13 +1,57 @@
-const johnnyFive = require("johnny-five");
+import johnnyFive, { Button } from "johnny-five";
 
 const board = new johnnyFive.Board();
 
-let led = null;
+let builtInLed: johnnyFive.Led;
+
+export interface IComponent {
+  pin: number;
+  instance: any;
+}
+
+export interface ILedComponent extends IComponent {
+  instance: johnnyFive.Led;
+}
+
+export interface IConnectionComponent extends IComponent {
+  instance: johnnyFive.Button;
+}
+
+export interface IConnectionLedMapping {
+  led: ILedComponent;
+  connection: IConnectionComponent;
+}
+
+let connectionLedMapping: IConnectionLedMapping[];
+
+const CONNECTION_PINS = [4, 5, 6, 7];
+const CONNECTION_LED_OFFSET = 4;
+
+function createConnectionLedMappingFromPin(
+  connectionPin: number
+): IConnectionLedMapping {
+  const ledPin = connectionPin + CONNECTION_LED_OFFSET;
+  const connection = new johnnyFive.Button({
+    isPullup: true,
+    pin: connectionPin
+  });
+  const led = new johnnyFive.Led(ledPin);
+
+  return {
+    led: {
+      pin: ledPin,
+      instance: led
+    },
+    connection: {
+      pin: connectionPin,
+      instance: connection
+    }
+  };
+}
 
 board.on("ready", () => {
-  console.log("connection");
-
-  led = new johnnyFive.Led(13);
+  builtInLed = new johnnyFive.Led(13);
+  connectionLedMapping = CONNECTION_PINS.map(createConnectionLedMappingFromPin);
 });
 
-export { board, led };
+export { board, connectionLedMapping, builtInLed };
--- src/socket/handler.ts
+++ src/socket/handler.ts
@@ -1,21 +1,76 @@
 import { Socket } from "socket.io";
-import { board, led } from "../controller/controller";
+import {
+  board,
+  builtInLed,
+  connectionLedMapping,
+  IConnectionLedMapping
+} from "../controller/controller";
+import { io } from "./socket";
 
 let ledState = false;
 let boardReady = false;
+const buttonState = false;
 
-board.on("ready", () => {
-  boardReady = true;
-});
+function emitButtonState() {
+  io.emit("button-state", {
+    state: buttonState
+  });
+}
+
+function emitConnectionState(pin: number, state: boolean) {
+  io.emit("connection-state", {
+    pin,
+    state
+  });
+}
+
+function toggleLed() {
+  if (boardReady && builtInLed) {
+    ledState = !ledState;
+
+    ledState ? builtInLed.on() : builtInLed.off();
+  }
+}
+
+function attachConnectionLedHandler({
+  connection,
+  led
+}: IConnectionLedMapping) {
+  let state = false;
+
+  connection.instance.on("down", () => {
+    if (state) {
+      return;
+    }
+
+    state = true;
+    emitConnectionState(connection.pin, true);
+    led.instance.on();
+    console.log(`Connection on pin ${connection.pin} connected`);
+  });
+
+  connection.instance.on("up", () => {
+    if (!state) {
+      return;
+    }
+
+    state = false;
+    emitConnectionState(connection.pin, false);
+    led.instance.off();
+    console.log(`Connection on pin ${connection.pin} disconnected`);
+  });
+}
 
 export function socketHandler(socket: Socket) {
-  socket.emit("test-forward", { hello: "world" });
+  io.emit("test-forward", { hello: "world" });
 
   socket.on("test-backward", data => {
-    if (boardReady && led) {
-      ledState = !ledState;
+    toggleLed();
+  });
 
-      ledState ? led.on() : led.off();
-    }
+  board.on("ready", () => {
+    boardReady = true;
+
+    connectionLedMapping.forEach(attachConnectionLedHandler);
   });
 }
